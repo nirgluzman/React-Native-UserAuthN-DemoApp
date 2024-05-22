f6007ed67b6b7227615a41c0eedb9449ef3c412b
nirgluzman <nir.gluzman@gmail.com>
2024-05-23 00:34:56 +0200

    create 'authenticate' to handle both signup and login, add AuthN token to response

--- util/auth.js
+++ util/auth.js
@@ -5,17 +5,27 @@ import axios from 'axios';
 
 const API_KEY = process.env.EXPO_PUBLIC_API_KEY;
 
+async function authenticate(mode, email, password) {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+
+  const response = await axios.post(url, {
+    email,
+    password,
+    returnSecureToken: true // should always be true.
+  });
+
+  const token = response.data.idToken; // Firebase Auth ID token for the authenticated user.
+  return token;
+}
+
 // Sign up with email/password
 // https://firebase.google.com/docs/reference/rest/auth#section-create-email-password
-export async function createUser(email, password) {
-  const response = await axios.post(
-    `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
-    {
-      email,
-      password,
-      returnSecureToken: true
-    }
-  );
+export function createUser(email, password) {
+  return authenticate('signUp', email, password);
+}
 
-  return response.data;
+// Sign in with email/password
+// https://firebase.google.com/docs/reference/rest/auth#section-sign-in-email-password
+export function loginUser(email, password) {
+  return authenticate('signInWithPassword', email, password);
 }
