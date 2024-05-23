8825085fccc331f53e50e6feb65f43e7aa7db5c4
nirgluzman <nir.gluzman@gmail.com>
2024-05-23 09:11:06 +0200

    add a fetch from Firebase database with token

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,12 +1,35 @@
 // Access to the WelcomeScreen requires user authentication.
 
+import { useState, useEffect, useContext } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// Context for managing user authentication
+import { AuthContext } from '../store/auth-context';
+
+// helper function for managing Firebase Realtime Database
+import { fetchMessage } from '../util/database';
+
 function WelcomeScreen() {
+  const [fetchedMessage, setFetchedMessage] = useState('');
+
+  // consuming the Context
+  const { token } = useContext(AuthContext);
+
+  useEffect(() => {
+    // fetch message from Firebase Realtime Database and set it in state.
+    async function getMessage() {
+      const message = await fetchMessage(token);
+      setFetchedMessage(message);
+    }
+
+    getMessage();
+  }, [token]);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
